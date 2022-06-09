fef56a9cb6cf6c7957af19381fbf8d1dada61cd7
E1psycongr00 <39326175+E1psycongr00@users.noreply.github.com>
2022-06-10 08:24:08 +0900

    feat: 유레카에 따른 FeignClient 설정
    - 더이상 url을 직접 지정 할 필요 x
    - name => application-name
    - Mapping(컨텍스트명/~~~~)

--- order/src/main/java/com/ecommerce/order/rest/customer/CustomerFeignClient.java
+++ order/src/main/java/com/ecommerce/order/rest/customer/CustomerFeignClient.java
@@ -6,10 +6,12 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "ecommerce-customer", url = "http://localhost:8076/ecommerce/customer")
+@FeignClient(name = "ecommerce-customer"
+//        url = "http://localhost:8076/ecommerce/customer"
+)
 public interface CustomerFeignClient {
     
-    @GetMapping(value="/rest/customers/{userid}")
+    @GetMapping(value="ecommerce/customer/rest/customers/{userid}")
     public Customer retrieveCustomer(@PathVariable("userid") String userid) throws Exception;
     
 }
