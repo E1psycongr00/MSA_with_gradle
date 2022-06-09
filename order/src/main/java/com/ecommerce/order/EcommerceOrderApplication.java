c48f6519b3d584c4d0c113904fe75d33e95671db
E1psycongr00 <39326175+E1psycongr00@users.noreply.github.com>
2022-06-10 08:23:08 +0900

    feat: Application에 @EnableEurekaClient 추가

--- order/src/main/java/com/ecommerce/order/EcommerceOrderApplication.java
+++ order/src/main/java/com/ecommerce/order/EcommerceOrderApplication.java
@@ -3,11 +3,13 @@ package com.ecommerce.order;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableFeignClients
 @EnableCircuitBreaker
+@EnableEurekaClient
 public class EcommerceOrderApplication {
 
 	public static void main(String[] args) {
