feaea7db54ff7096ac1c26558cfeeedc2a63c203
E1psycongr00 <39326175+E1psycongr00@users.noreply.github.com>
2022-06-13 09:34:53 +0900

    feat: 네트워크 통신을 위해서는 직렬화 가능 객체여야 한다 implement Serializable

---
+++ kafka_subscriber/src/main/java/com/kafka/subscriber/domain/entity/Ping.java
@@ -0,0 +1,41 @@
+package com.kafka.subscriber.domain.entity;
+
+import java.io.Serializable;
+
+// 반드시 직렬화 객체로 만들어야 된다.  ==> implements Serializable 하면 됨.
+public class Ping implements Serializable {
+	
+	String mesg;
+	String name;
+	
+	public Ping() {
+	}
+
+	public Ping(String mesg, String name) {
+		super();
+		this.mesg = mesg;
+		this.name = name;
+	}
+
+	public String getMesg() {
+		return mesg;
+	}
+
+	public void setMesg(String mesg) {
+		this.mesg = mesg;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	@Override
+	public String toString() {
+		return "Ping [mesg=" + mesg + ", name=" + name + "]";
+	}
+
+}
